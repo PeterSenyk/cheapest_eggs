43971c939fe07f9ae48398cadf7c54decd6205b6
Jaiden Duncan <jdduncan0614@gmail.com>
2023-11-05 20:30:45 -0800

    Add search functionality
    Adds the search functionality, as well as
    the beginning of sort functionality

---
+++ scripts/search.js
@@ -0,0 +1,30 @@
+// Adds the search function to the search button
+$("#searchBtn").on("click", function(){
+    var search = $("#search").val();
+    var url = "search_result.html?q=" + search;
+    
+        let sort = document.getElementsByName("sort");
+        for (var i = 0; i < url.length; i++) {
+            if (sort[i].checked) {
+                url += "&sort=" + sort[i].value;
+                break;
+            }
+        }
+        let order = document.getElementsByName("order");
+        for (var i = 0; i < order.length; i++) {
+            if (order[i].checked) {
+                url += "&order=" + order[i].value;
+            } 
+        }
+    window.location.href = url
+})
+$("#search").on("click", function(){
+    $("#search_dropdown").css("display", "block")
+    $("body").css({"background-color":"grey"})
+    $("main").css("opacity", "0.5")
+}) 
+$("#close").on("click", function(){
+    $("#search_dropdown").css("display", "none")
+    $("body").css({"background-color":"white"})
+    $("main").css("opacity", "1")
+}) 
