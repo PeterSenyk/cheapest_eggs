96eec758a312b62e1fcb01822fa7bec7bfa0920a
Cai Chun Yan <caichunyan17@gmail.com>
2023-11-06 17:57:25 -0800

    Update commenting

--- scripts/db_product_add.js
+++ scripts/db_product_add.js
@@ -57,35 +57,26 @@ function writeProducts() {
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
 function displayCardsDynamically(collection) {
-    let cardTemplate = document.getElementById("product_card_template"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+    let cardTemplate = document.getElementById("product_card_template"); 
 
-    db.collection(collection).get()   //the collection called "hikes"
+    db.collection(collection).get()   
         .then(allProducts => {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allProducts.forEach(doc => { //iterate thru each doc
-                var title = doc.data().produce_name;       // get value of the "name" key
-                var details = doc.data().details;  // get value of the "details" key
-                var pluCode = doc.data().plu_code;    //get unique ID to each hike to be used for fetching right image
-                var productPrice = doc.data().price; //gets the length field
-                var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
-                //update title and text and image
+            allProducts.forEach(doc => { 
+                var title = doc.data().produce_name;     
+                var details = doc.data().details;  
+                var pluCode = doc.data().plu_code;   
+                var productPrice = doc.data().price; 
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); 
+
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-length').innerHTML = productPrice + " CAD";
                 newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('.card-image').src = `./images/${pluCode}.png`; //Example: NV01.jpg
+                newcard.querySelector('.card-image').src = `./images/${pluCode}.png`; 
                 newcard.querySelector('a').href = "eachProduct.html?docID=" + docID;
 
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-                //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-
-                //i++;   //Optional: iterate variable to serve as unique ID
             })
         })
 }
