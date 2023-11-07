2575c6a8216697096c2997b14f859882938e1ee5
PeterSenyk <Peter.Senyk@gmail.com>
2023-11-06 18:49:01 -0800

    FIX data upload

--- scripts/share.js
+++ scripts/share.js
@@ -1,30 +1,35 @@
 const product = document.getElementById("product");
 const price = document.getElementById("price");
 const amount = document.getElementById("amount");
-// const location = document.getElementById("location");  #Javascript does not like this ??
+const locationField = document.getElementById("location");  
 const photo = document.getElementById("photo");
 const form = document.getElementById("share-price-form");
 
+
 // add a product to the database
-form.addEventListener("submit", (e) =>
-{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    db.collection("pending_uploads").add({
-        product: form.product.value,
-        price: form.price.value,
-        amount: form.amount.value,
-        location: form.location.value,
-        photo: form.photo.value,
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()
-    });
-    form.product.value = "";
-    form.price.value = "";
-    form.amount.value = "";
-    form.location.value = "";
-    form.photo.value = "";
+    const productField = product.value
+    const priceField = price.value
+    const amountField = amount.value
+    const locationBox = locationField.value
+    const userId = firebase.auth().currentUser.uid
+    const timeStamp = new Date().toISOString();
+    db.collection("pending_uploads").doc(userId).collection("user_uploads").doc(timeStamp).set({
+        
+        product: productField,
+        price: priceField,
+        amount: amountField,
+        location: locationBox,
+        last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+        user_id: userId,
+    }, { merge: true })
+        .then(() => {
+            // This will execute after the set() operation is successful
+            window.location = "./share_review.html";
+        })
+        .catch((error) => {
+            // Handle any errors here
+            console.error("Error writing document: ", error);
+        });
 })
-
-function btnClicked(){
-    document.location.href="./share_review.html"
-}
-
