3351814cfe37ec0cf4c043dfcf5876c41e6a516b
Cai Chun Yan <caichunyan17@gmail.com>
2023-11-14 14:32:56 -0800

    Create js to add more data to db

--- scripts/db_product_add.js
+++ scripts/db_product_add.js
@@ -3,44 +3,41 @@ function writeProducts() {
     //define a variable for the collection you want to create in Firestore to populate data
     var products_ref = db.collection("products");
 
-    var products_detail_ref = products_ref.doc('banana').collection('details');
+    var products_detail_ref = products_ref.doc('watermelon').collection('details'); // change doc('name') based on item you inject
 
     products_detail_ref.add({
-        plu_code: 4011,
-        produce_name: "Banana",
-        price: 0.79,
-        variety: "yellow",
-        size: 'medium',
-        net_price: "per lb",
-        store: "Safeway",
-        city: "Burnaby",
-        province: "BC",
-        postal_code: "V5A 1S6",
-        details: "beautiful yellow banana",
+        plu_code: 3421,
+        produce_name: "Watermelon",
+        price: 3.79,
+        variety: "Seedless", // seedless, seeded, yellow, orange, etc
+        size: 'Mini',   // mini, small, medium, large, extra large
+        net_price: "per lb", // per lb, per item
+        store: "Price Smart", // store name
+        city: "Burnaby",  // city name
+        province: "BC", // province name
+        postal_code: "V5A 1S6", // postal code
         last_updated: firebase.firestore.FieldValue.serverTimestamp(),  //current system time
         sale_date: firebase.firestore.Timestamp.fromDate(new Date("December 10, 2020")),
-        good_deal: true,
+        good_deal: true, // true of false just make it random
     })
 
     products_detail_ref.add({
-        plu_code: 4234,
-        produce_name: "Banana",
-        price: 1.29,
-        variety: "baby",
-        size: 'small',
+        plu_code: 3308,
+        produce_name: "Watermelon",
+        price: 6.99,
+        variety: "Orange",
+        size: 'All Sizes',
         net_price: "per lb",
-        store: "Safeway",
+        store: "Whole Foods",
         city: "Burnaby",
         province: "BC",
         postal_code: "V5A 1S6",
-        details: "really fresh",
+        details: "Placeholder details",
         last_updated: firebase.firestore.FieldValue.serverTimestamp(),  //current system time
         sale_date: firebase.firestore.Timestamp.fromDate(new Date("December 10, 2020")),
-        good_deal: true,
+        good_deal: false,
     })
 }
-
-writeProducts();
 
 
 function writeProducts_doc() {
