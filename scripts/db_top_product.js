5abccc59de27c99ed39821a65a7ba25e98b2ee77
Yifei Zeng <yzeng33@my.bcit.ca>
2023-11-20 01:30:01 -0800

    Show random cheap deals

--- scripts/db_top_product.js
+++ scripts/db_top_product.js
@@ -1,14 +1,20 @@
 function display_sale_products(collection) {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in, only signed in users can see add button
+        const items = ['apple', 'banana', 'lettuce', 'kiwi', 'tomato', 'watermelon']
+        random1 = Math.floor(Math.random() * items.length)
+        random2 = Math.floor(Math.random() * items.length)
+        random3 = Math.floor(Math.random() * items.length)
+        random4 = Math.floor(Math.random() * items.length)
+
         if (user) {
             db.collection(collection).get().then(allProducts => {
                 allProducts.forEach(doc => {
-                    // apple is placeholder for now
-                    if (doc.id == 'apple') {
-                        db.collection(collection).doc('apple').collection('details').get().then(allProducts => {
+                    if (doc.id == items[random1]) {
+                        db.collection(collection).doc(items[random1]).collection('details').orderBy("price").limit(1).get().then(allProducts => {
                             allProducts.forEach(doc => {
                                 if (doc.data().good_deal == true) {
+                                    var productName = doc.data().produce_name;
                                     var pluCode = doc.data().plu_code;
                                     var productPrice = doc.data().price;
                                     document.getElementById('product-goes-here').innerHTML += (
@@ -17,7 +23,37 @@ function display_sale_products(collection) {
                                     <div class='img_container'>
                                         <img src =./images/${pluCode}.png alt="cover">
                                     </div>
-                                    <p>Price ${productPrice}</p>
+                                    <p>${productName}</p>
+                                    <p>Price: ${productPrice}$</p>
+                                    <button>Add</button>
+                                </div>
+                                `
+                                    )
+                                }
+                            })
+                        })
+                    }
+
+                })
+            })
+
+            db.collection(collection).get().then(allProducts => {
+                allProducts.forEach(doc => {
+                    if (doc.id == items[random2]) {
+                        db.collection(collection).doc(items[random2]).collection('details').orderBy("price").limit(1).get().then(allProducts => {
+                            allProducts.forEach(doc => {
+                                if (doc.data().good_deal == true) {
+                                    var productName = doc.data().produce_name;
+                                    var pluCode = doc.data().plu_code;
+                                    var productPrice = doc.data().price;
+                                    document.getElementById('product-goes-here').innerHTML += (
+                                        `
+                                <div class="sale_product_card">
+                                    <div class='img_container'>
+                                        <img src =./images/${pluCode}.png alt="cover">
+                                    </div>
+                                    <p>${productName}</p>
+                                    <p>Price: ${productPrice}$</p>
                                     <button>Add</button>
                                 </div>
                                 `
@@ -28,6 +64,64 @@ function display_sale_products(collection) {
                     }
                 })
             })
+
+            db.collection(collection).get().then(allProducts => {
+                allProducts.forEach(doc => {
+                    if (doc.id == items[random3]) {
+                        db.collection(collection).doc(items[random3]).collection('details').orderBy("price").limit(1).get().then(allProducts => {
+                            allProducts.forEach(doc => {
+                                if (doc.data().good_deal == true) {
+                                    var productName = doc.data().produce_name;
+                                    var pluCode = doc.data().plu_code;
+                                    var productPrice = doc.data().price;
+                                    document.getElementById('product-goes-here').innerHTML += (
+                                        `
+                                <div class="sale_product_card">
+                                    <div class='img_container'>
+                                        <img src =./images/${pluCode}.png alt="cover">
+                                    </div>
+                                    <p>${productName}</p>
+                                    <p>Price: ${productPrice}$</p>
+                                    <button>Add</button>
+                                </div>
+                                `
+                                    )
+                                }
+                            })
+                        })
+                    }
+
+                })
+            })
+
+            db.collection(collection).get().then(allProducts => {
+                allProducts.forEach(doc => {
+                    if (doc.id == items[random4]) {
+                        db.collection(collection).doc(items[random4]).collection('details').orderBy("price").limit(1).get().then(allProducts => {
+                            allProducts.forEach(doc => {
+                                if (doc.data().good_deal == true) {
+                                    var productName = doc.data().produce_name;
+                                    var pluCode = doc.data().plu_code;
+                                    var productPrice = doc.data().price;
+                                    document.getElementById('product-goes-here').innerHTML += (
+                                        `
+                                <div class="sale_product_card">
+                                    <div class='img_container'>
+                                        <img src =./images/${pluCode}.png alt="cover">
+                                    </div>
+                                    <p>${productName}</p>
+                                    <p>Price: ${productPrice}$</p>
+                                    <button>Add</button>
+                                </div>
+                                `
+                                    )
+                                }
+                            })
+                        })
+                    }
+                })
+            })
+
         } else {
             db.collection(collection).get().then(allProducts => {
                 allProducts.forEach(doc => {
