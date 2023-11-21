552c11e6886f33398a9b582b53c34481d9a65c9b
Peter Senyk <Peter.Senyk@gmail.com>
2023-11-20 21:45:33 -0800

    FIX search for user shared products

--- scripts/db_product_search.js
+++ scripts/db_product_search.js
@@ -1,5 +1,9 @@
 console.log('db_product_search.js loaded');
 sort_method = undefined
+
+
+var search_item;
+
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
@@ -34,7 +38,7 @@ function displayCardsDynamically(collection) {
                         newcard.querySelector('.card-href').addEventListener("click", add_to_list_from_search);
 
                         document.getElementById(collection + "-go-here").appendChild(newcard);
-                    
+
                     })
                 })
             }
@@ -43,17 +47,55 @@ function displayCardsDynamically(collection) {
 }
 
 
-async function displayUserShared(search_item) {
-    let query = db.collectionGroup("user_uploads").where("product", "==", "search_item")
-    query.get().then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-            console.log(doc.id, " => ", doc.data());
-        });
-    }).catch(error => {
-        console.error("Error getting documents: ", error);
-    });
+function getSharedProducts(search_item) {
+    console.log('getSharedProducts called with item:', search_item);
+    const db = firebase.firestore(); // Ensure Firebase is initialized
 
+    db.collectionGroup("user_uploads") // This will search across all 'user_uploads' subcollections in the database
+        .where("product", "==", search_item)
+        .get()
+        .then((querySnapshot) => {
+            const sharedDetails = [];
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                sharedDetails.push(doc.data());
+            });
+            // Once all documents are added to sharedDetails, call the display function
+            displaySharedProducts(sharedDetails, search_item); // Pass search_item if needed for further use
+        })
+        .catch((error) => {
+            console.error("Error getting documents: ", error);
+        });
 }
+
+function displaySharedProducts(sharedDetails, search_item) {
+    const template = document.getElementById('shared_product_card_template'); // Use the correct template ID
+    const container = document.getElementById('shared-products-go-here'); // Use the correct container ID
+
+    container.innerHTML = ''; // Clear existing cards
+
+    sharedDetails.forEach(detail => {
+        let newcard = template.content.cloneNode(true);
+
+        // Update selectors to match your template's classes or IDs
+        newcard.querySelector('.share-card-title').textContent = detail.product || 'No title';
+        newcard.querySelector('.share-card-length').textContent = detail.price + " CAD" || 'No price';
+        newcard.querySelector('.share-card-text').textContent = detail.variety || 'No details';
+        newcard.querySelector('.share-card-image').src = detail.photo || './images/noimg.png';
+
+        // Add event listener
+        let addButton = newcard.querySelector('.card-href');
+        addButton.setAttribute("id", `${detail.id} ${detail.produce_name}`);
+        addButton.addEventListener("click", function() {
+            add_to_list_from_search(detail.id, search_item); // Make sure this function is defined
+        });
+
+        container.appendChild(newcard);
+    });
+}
+
+
+
 
 // eventlistener search
 searchButton = document.getElementById('search_button');
@@ -66,14 +108,18 @@ searchButton.addEventListener('click', function () {
 });
 queryParams = new URLSearchParams(window.location.search);
 search_item = queryParams.get('search_item');
-displayCardsDynamically('products')
+displayCardsDynamically('products');
 add_to_search_history(search_item);
+if (search_item) {
+    getSharedProducts(search_item);
+};
 
 // eventlistener sort
 sort_button = document.getElementById('sort_button');
 sort_button.addEventListener('click', function () {
     sort_method = undefined
-    displayCardsDynamically('products')
+    displayCardsDynamically('products');
+    getSharedProducts(search_item);
 })
 
 sort_button_desc = document.getElementById('sort_button_desc');
@@ -81,15 +127,16 @@ sort_button_desc.addEventListener('click', function () {
     sort_method = 'desc'
     console.log(sort_method)
     displayCardsDynamically('products')
+    getSharedProducts(search_item);
 })
 
-function add_to_search_history(search_item){
+function add_to_search_history(search_item) {
     var user = firebase.auth().currentUser;
     var uid = user.uid;
     userDoc = db.collection('users').doc(uid);
     userDoc.update({
         search_history: firebase.firestore.FieldValue.arrayUnion(search_item)
-    }).catch(function(error){
+    }).catch(function (error) {
         console.log(error);
     })
 }
