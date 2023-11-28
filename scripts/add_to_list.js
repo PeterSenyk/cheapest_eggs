1bfbaa7e0b199c6bd3bd26fd03a1d4445f7bdc64
Jaiden Duncan <jdduncan0614@gmail.com>
2023-11-27 16:30:55 -0800

    Remove redundant lines

--- scripts/add_to_list.js
+++ scripts/add_to_list.js
@@ -1,12 +1,12 @@
-// this function adds the corresponding item to the list from the search_results page
+// ---------------------------------------------------------------------------------------
+// Add the corresponding item to the list.
+// ---------------------------------------------------------------------------------------
 async function add_to_list_from_search(path, isShared) {
     const uid = localStorage.getItem("uid");
     const userList = db.collection("users").doc(uid).collection("user_list");
     const querySnapshot = await userList.where("path", "==", path).get();
     
     if (!querySnapshot.empty) {
-        console.log("item already in list");
-
         Swal.fire({
             toast: true,
             position: 'bottom',
@@ -17,8 +17,6 @@ async function add_to_list_from_search(path, isShared) {
             timerProgressBar: true,
         });
     } else {
-        console.log("item added to list");
-        
         userList.doc().set({
             path: path,
             quantity: 1,
@@ -42,23 +40,4 @@ firebase.auth().onAuthStateChanged(user => {
     if (user) {
         localStorage.setItem("uid", user.uid);
     }
-    else {
-        // todo
-    }
 });
-
-addItems = function() {
-    uid = localStorage.getItem("uid");
-    console.log("adding items");
-
-    db.collection("users").doc(uid).collection("user_list").add({
-        produce_name: "apple",
-        itemid: "ZX6yRha1zmpdZuroC8OI",
-        quantity: 1
-    })
-    db.collection("users").doc(uid).collection("user_list").add({
-        produce_name: "banana",
-        itemid: "0z5R4483cwJB0TcPQJZ3",
-        quantity: 1
-    })
-}
