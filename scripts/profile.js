a51b5da275a4c481ff797cdcc079743f738c7f0a
Yifei Zeng <yzeng33@my.bcit.ca>
2023-11-30 22:22:51 -0800

    Add comments in profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,84 +1,94 @@
-var currentUser;               //points to the document of the user who is logged in
+// Global variable to store the document reference of the logged-in user
+let currentUser;
+
+// Function to populate user information on the profile page
 function populateUserInfo() {
+    // Firebase authentication state change listener
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in
         if (user) {
-            //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").doc(user.uid)
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    //get the data fields of the user
-                    var userName = userDoc.data().name;
-                    var userCountry = userDoc.data().country;
-                    var userCity = userDoc.data().city;
-                    var picUrl = userDoc.data().profilePic;
+            // Reference to the user document using the user's UID
+            currentUser = db.collection("users").doc(user.uid);
 
-                    //if the data fields are not empty, then write them in to the form.
-                    if (userName != null) {
-                        document.getElementById("nameInput").value = userName;
-                    }
-                    if (userCountry != null) {
-                        document.getElementById("countryInput").value = userCountry;
-                    }
-                    if (userCity != null) {
-                        document.getElementById("cityInput").value = userCity;
-                    }
-                    if (picUrl != null) {
-                        document.getElementById("mypic-goes-here").src = picUrl;
-                    }
-                })
+            // Retrieve and populate user information
+            currentUser.get().then(userDoc => {
+                // Extract user data fields
+                const userName = userDoc.data().name;
+                const userCountry = userDoc.data().country;
+                const userCity = userDoc.data().city;
+                const picUrl = userDoc.data().profilePic;
+
+                // Populate form fields if data fields are not empty
+                if (userName != null) {
+                    document.getElementById("nameInput").value = userName;
+                }
+                if (userCountry != null) {
+                    document.getElementById("countryInput").value = userCountry;
+                }
+                if (userCity != null) {
+                    document.getElementById("cityInput").value = userCity;
+                }
+                if (picUrl != null) {
+                    document.getElementById("mypic-goes-here").src = picUrl;
+                }
+            });
         } else {
-            // No user is signed in.
+            // No user is signed in
             console.log("No user is signed in");
         }
     });
 }
 
-//call the function to run it 
+// Call the function to populate user information
 populateUserInfo();
 
+// Function to enable form fields for editing user information
 function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
-    //Change button colors
+
+    // Change button colors for visual indication
     document.querySelector('.save').classList.remove('btn-secondary');
     document.querySelector('.save').classList.add('btn-success');
     document.querySelector('.edit').classList.remove('btn-success');
     document.querySelector('.edit').classList.add('btn-secondary');
 }
 
+// Function to save edited user information
 function saveUserInfo() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        var storageRef = storage.ref("images/" + user.uid + ".jpg");
+    firebase.auth().onAuthStateChanged(user => {
+        // Reference to the storage for user profile pictures
+        const storageRef = storage.ref("images/" + user.uid + ".jpg");
 
-        //Asynch call to put File Object (global variable ImageFile) onto Cloud
-        storageRef.put(ImageFile)
-            .then(function () {
-                //Asynch call to get URL from Cloud
-                storageRef.getDownloadURL()
-                    .then(function (url) { // Get "url" of the uploaded file
-                        userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-                        userCountry = document.getElementById('countryInput').value;     //get the value of the field with id="schoolInput"
-                        userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
-                        //Asynch call to save the form fields into Firestore.
-                        db.collection("users").doc(user.uid).update({
-                            name: userName,
-                            country: userCountry,
-                            city: userCity,
-                            profilePic: url // Save the URL into users collection
-                        })
-                            .then(function () {
-                                document.getElementById('personalInfoFields').disabled = true;
-                            })
-                    })
-            })
-    })
-    //Change button colors
+        // Asynchronous call to upload File Object (global variable ImageFile) to Cloud Storage
+        storageRef.put(ImageFile).then(() => {
+            // Asynchronous call to get URL from Cloud Storage
+            storageRef.getDownloadURL().then(url => {
+                // Get form field values
+                const userName = document.getElementById('nameInput').value;
+                const userCountry = document.getElementById('countryInput').value;
+                const userCity = document.getElementById('cityInput').value;
+
+                // Asynchronous call to update form fields in Firestore
+                currentUser.update({
+                    name: userName,
+                    country: userCountry,
+                    city: userCity,
+                    profilePic: url
+                }).then(() => {
+                    // Disable form fields after saving
+                    document.getElementById('personalInfoFields').disabled = true;
+                });
+            });
+        });
+    });
+
+    // Change button colors back to default
     document.querySelector('.save').classList.remove('btn-success');
     document.querySelector('.save').classList.add('btn-secondary');
     document.querySelector('.edit').classList.remove('btn-secondary');
     document.querySelector('.edit').classList.add('btn-success');
+
     // Alert the user that their profile has been updated
     Swal.fire({
         title: "Saved",
@@ -87,30 +97,29 @@ function saveUserInfo() {
     });
 }
 
+// Function to fetch and display additional user information
 function getOtherInfoFromDB() {
-    // Check if the currentUser variable is set
     firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
+        // Check if user is signed in
         if (user) {
             // Retrieve user information based on their UID
-            db.collection("users").doc(user.uid).get()
-                .then((doc) => {
-                    if (doc.exists) {
-                        var email = doc.data().email;
-                        var score = doc.data().score;
-                        var title = doc.data().title;
+            db.collection("users").doc(user.uid).get().then(doc => {
+                if (doc.exists) {
+                    // Extract additional user information
+                    const email = doc.data().email;
+                    const score = doc.data().score;
+                    const title = doc.data().title;
 
-                        // Update the elements with the retrieved values
-                        $("#email").text(email);
-                        $("#score").text(score);
-                        $("#title").text(title);
-                    } else {
-                        console.log("User document with UID " + user.uid + " does not exist.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Error getting data from Firestore: ", error);
-                });
+                    // Update elements with retrieved values
+                    $("#email").text(email);
+                    $("#score").text(score);
+                    $("#title").text(title);
+                } else {
+                    console.log("User document with UID " + user.uid + " does not exist.");
+                }
+            }).catch(error => {
+                console.error("Error getting data from Firestore: ", error);
+            });
         } else {
             // Handle the case when currentUser is not set
             console.log("No user is currently signed in.");
@@ -118,45 +127,51 @@ function getOtherInfoFromDB() {
     });
 }
 
-getOtherInfoFromDB(); // Run the function to fetch and update data
+// Run the function to fetch and update additional user information
+getOtherInfoFromDB();
 
-// Add a click event listener to the sign out button
-document.getElementById("sign-out").addEventListener("click", function () {
+// Add click event listener to the sign-out button
+document.getElementById("sign-out").addEventListener("click", () => {
     // Call the logout function when the button is clicked
     logout();
+
     // Redirect to "index.html"
     window.location.href = "index.html";
 });
 
-// Add a click event listener to the about button
-document.getElementById("about").addEventListener("click", function () {
+// Add click event listener to the about button
+document.getElementById("about").addEventListener("click", () => {
     // Redirect to "about.html"
     window.location.href = "about.html";
 });
 
-var ImageFile;      //global variable to store the File Object reference
+// Global variable to store the File Object reference for user profile picture
+let ImageFile;
 
+// Function to handle file input and update preview
 function chooseFileListener() {
-    const fileInput = document.getElementById("mypic-input");   // pointer #1
-    const image = document.getElementById("mypic-goes-here");   // pointer #2
+    const fileInput = document.getElementById("mypic-input"); // Pointer #1
+    const image = document.getElementById("mypic-goes-here"); // Pointer #2
 
-    //attach listener to input file
-    //when this file changes, do something
-    fileInput.addEventListener('change', function (e) {
-
-        //the change event returns a file "e.target.files[0]"
+    // Attach listener to input file
+    // When this file changes, do something
+    fileInput.addEventListener('change', e => {
+        // The change event returns a file "e.target.files[0]"
         ImageFile = e.target.files[0];
-        var blob = URL.createObjectURL(ImageFile);
+        const blob = URL.createObjectURL(ImageFile);
 
-        //change the DOM img element source to point to this file
-        image.src = blob;    //assign the "src" property of the "img" tag
-    })
+        // Change the DOM img element source to point to this file
+        image.src = blob; // Assign the "src" property of the "img" tag
+    });
 }
+
+// Run the function to enable file input listener
 chooseFileListener();
 
+// Function to populate share card section on the page
 function populateShare() {
-    let shareCardTemplate = document.getElementById("shareCardTemplate");
-    let shareCardGroup = document.getElementById("shareCardGroup");
+    const shareCardTemplate = document.getElementById("shareCardTemplate");
+    const shareCardGroup = document.getElementById("shareCardGroup");
 
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
@@ -184,6 +199,7 @@ function populateShare() {
                     shareCard.querySelector(".amount").innerHTML = `Amount: ${amount}`;
                     shareCard.querySelector(".storeName").innerHTML = `StoreName: ${storeName}`;
                     shareCard.querySelector(".address").innerHTML = `Address: ${address}`;
+                    // If there is a photo, set the src attribute of the photo element to the photo URL
                     if (photo) {
                         shareCard.querySelector(".photo").src = photo
                     }
@@ -196,4 +212,5 @@ function populateShare() {
     });
 }
 
+// Run the function to populate share cards
 populateShare();
