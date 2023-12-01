cd8437b68f5e838c69d0528a0858a00eef59492b
Jaiden Duncan <jdduncan0614@gmail.com>
2023-11-30 22:51:04 -0800

    Add case for no logged in user to add_to_list.js

--- scripts/add_to_list.js
+++ scripts/add_to_list.js
@@ -2,8 +2,23 @@
 // Add the corresponding item to the list.
 //--------------------------------------------------
 async function add_to_list_from_search(path, isShared) {
+    // get uid
     const uid = localStorage.getItem("uid");
+    if (uid == 0) { // uses loose equality
+        Swal.fire({
+            // display error popup
+            showConfirmButton: false,
+            timer: 1000,
+            title: "Not signed in",
+            text: "Please sign in to add items to your list!",
+            icon: "error",
+            timerProgressBar: true,
+        });
+        return;
+    }
+    // get user list reference
     const userList = db.collection("users").doc(uid).collection("user_list");
+    // check if item already exists in list
     const querySnapshot = await userList.where("path", "==", path).get();
     // check for document existence
     if (!querySnapshot.empty) {
@@ -18,10 +33,11 @@ async function add_to_list_from_search(path, isShared) {
             timerProgressBar: true,
         });
     } else {
+        // add item to list
         userList.doc().set({
-            path: path,
+            path, // uses shorthand property name syntax
             quantity: 1,
-            isShared: isShared,
+            isShared, // uses shorthand property name syntax
         })
         // display success toast
         Swal.fire({
@@ -39,5 +55,7 @@ async function add_to_list_from_search(path, isShared) {
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         localStorage.setItem("uid", user.uid);
+    } else {
+        localStorage.setItem("uid", 0);
     }
 });
